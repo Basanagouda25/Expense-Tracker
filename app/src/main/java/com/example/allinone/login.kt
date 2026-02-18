8678a1169f7a70921816ca22837d3922393c6cd2
Basan gouda <you@example.com>
2026-02-19 00:12:38 +0530

    First commit

---
+++ app/src/main/java/com/example/allinone/login.kt
@@ -0,0 +1,95 @@
+package com.example.allinone
+
+import android.widget.Space
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun login(){
+    // remember is used because compose will remember this value after every recomposition
+    // mutableStateOf is used to observe the compose if it is changed the values in ui automatically changes
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    var message by remember { mutableStateOf("") }
+
+    // now the ui for jetpack compose
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            "Login Screen",
+            fontSize = 28.sp,
+            fontFamily = FontFamily.SansSerif
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        //input email feild
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            label = {Text("Enter Email")}, //  this is like a placeholder
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            label = {Text("Enter password")},
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick ={
+                if(email == "user@gmail.com" && password == "user@123"){
+                    message = "Login Sucessful"
+                }else{
+                    message = "Login Failed"
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Login")
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = message,
+            fontSize = 18.sp
+        )
+    }
+}
+
+@Preview
+@Composable
+fun loginPreview(){
+    login()
+}
